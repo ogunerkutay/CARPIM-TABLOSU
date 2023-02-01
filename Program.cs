4a5976966a52550240ad45c1bc1eb0ffdd9391f7
OGÜN ERKUTAY <77350593+ogunerkutay@users.noreply.github.com>
2023-02-01 18:08:30 +0300

    Konsol ekranına çarpım tablosunu yazan örnek uygulama.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CARPIM_TABLOSU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string satir = string.Empty;
+            for (int i = 1; i <11; i++)
+            {
+                for (int j = 1; j < 11; j++)
+                {
+                    satir = satir + $"{j} x {i} = {j*i}    ";
+                }
+                Console.WriteLine(satir);
+                satir = string.Empty;
+            }
+        }
+    }
+}
